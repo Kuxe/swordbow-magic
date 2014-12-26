98ef610cd37b0afbc9780b32efa8275e5b589c9d
Joakim Thoren <joakimthoren93@gmail.com>
2014-12-26 18:19:53 +0100

    updated test for movecomponent

--- test/test_movecomponent.cpp
+++ test/test_movecomponent.cpp
@@ -6,7 +6,7 @@ TEST_CASE("MoveComponent") {
 	SECTION("Initialization") {
 		REQUIRE(mc.xpos == 0);
 		REQUIRE(mc.ypos == 0);
-		REQUIRE(mc.xspeed == 1);
-		REQUIRE(mc.yspeed == 1);
+		REQUIRE(mc.xspeed == 100.0f);
+		REQUIRE(mc.yspeed == 100.0f);
 	}
 }
