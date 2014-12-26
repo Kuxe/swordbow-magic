c7f78ca0214b7af94ecc7467d4dade7e3bcf684e
Joakim Thoren <joakimthoren93@gmail.com>
2014-12-26 19:03:22 +0100

    fixed #10, created a spatial-indexer

---
+++ include/gridindexer.h
@@ -0,0 +1,44 @@
+#ifndef GRIDINDEXER_H
+#define GRIDINDEXER_H
+
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+class MoveComponent;
+class ComponentManager;
+
+class GridIndexer {
+ public:
+
+struct CellIndex {
+	unsigned char xcell;
+	unsigned char ycell;
+};
+
+//Each cell has some ids associated with movecomponents
+//(which are getted from componentmanager)
+struct Cell {
+	set<unsigned long long int*> ids;
+};
+
+	static constexpr unsigned char SIZE = 255;
+	static constexpr unsigned char CELL_SIDE = 20;
+
+	set<unsigned long long int*> ids;
+	unordered_map<unsigned long long int*, CellIndex> cellIndicies;
+	
+ 	Cell cells[SIZE][SIZE];
+ 	ComponentManager* const componentManager;
+
+	GridIndexer(ComponentManager* componentManager);
+
+	void add(unsigned long long int* id);
+	void remove(unsigned long long int* id);
+	void index(unsigned long long int* id);
+	void update();
+	set<unsigned long long int*> getCell(unsigned char x, unsigned char y) const;
+};
+
+#endif //GRIDINDEXER_H
---
+++ src/gridindexer.cpp
@@ -0,0 +1,85 @@
+#include "gridindexer.h"
+#include "movecomponent.h"
+#include "componentmanager.h"
+#include <iostream>
+
+using namespace std;
+
+GridIndexer::GridIndexer(ComponentManager* componentManager)
+	: componentManager(componentManager) {
+
+}
+
+void GridIndexer::add(unsigned long long int* id) {
+	ids.insert(id);
+
+	const auto moveComponent = componentManager->moveComponents.at(id);
+
+	const unsigned char cellx = moveComponent->xpos / CELL_SIDE;
+	const unsigned char celly = moveComponent->ypos / CELL_SIDE;
+
+	//Remember cellindex
+	cellIndicies.insert(
+		make_pair(
+			id,
+			CellIndex{
+				cellx,
+				celly
+			}
+		)
+	);
+
+	//Add id to the cell
+	cells[cellx][celly].ids.insert(id);
+
+	index(id);
+}
+
+void GridIndexer::remove(unsigned long long int* id) {
+	const unsigned char cellX = cellIndicies.at(id).xcell;
+	const unsigned char cellY = cellIndicies.at(id).ycell;
+
+	//The cell doesnt contain this id anymore...
+	cells[cellX][cellY].ids.erase(id);
+
+	//The gridindexer doesnt need to remember previous cellindex for id
+	cellIndicies.erase(id);
+
+	//The gridindexer doesnt contain this id anymore...
+	ids.erase(id);
+}
+
+void GridIndexer::index(unsigned long long int* id) {
+	const auto mc = componentManager->moveComponents.at(id);
+	const unsigned char oldCellX = cellIndicies.at(id).xcell;
+	const unsigned char oldCellY = cellIndicies.at(id).ycell;
+	const unsigned char curCellX = mc->xpos / CELL_SIDE;
+	const unsigned char curCellY = mc->ypos / CELL_SIDE;
+
+	//If unit has moved to another cell
+	if(	curCellX != oldCellX || curCellY != oldCellY) {
+		//id has moved into another cell, so needs to re-index
+
+		//Remove from id old cell
+		cells[oldCellX][oldCellY].ids.erase(id);
+
+		//Add id to new cell
+		cells[curCellX][curCellY].ids.insert(id);
+
+		//update cellindicies
+		cellIndicies.at(id).xcell = curCellX;
+		cellIndicies.at(id).ycell = curCellY;
+	}
+}
+
+void GridIndexer::update() {
+	for(auto id : ids) {
+		index(id);
+	}
+}
+
+set<unsigned long long int*> GridIndexer::getCell(
+	const unsigned char x,
+	const unsigned char y) const {
+	return cells[x][y].ids;
+}
