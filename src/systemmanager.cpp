4586096c78616d97a489f14328d017edd72dec44
Joakim Thorén <joakimthoren93@gmail.com>
2015-03-08 19:34:53 +0100

    added another error message to systemmanager getSystem()

--- src/systemmanager.cpp
+++ src/systemmanager.cpp
@@ -43,6 +43,7 @@ ISystem* SystemManager::getSystem(string identifier) {
 		cout << "1. Maybe there as a typo in the system-identifier argument, " << identifier << "?" << endl;
 		cout << "2. Maybe " << identifier << " wasn't added to this SystemManager?" << endl;
 		cout << "3. Maybe " << identifier << " was added, but got removed?" << endl;
+		cout << "4. Maybe another system didn't implement getIdentifier() (in that case, above error messages are misleading!)" << endl;
 		cout << "Things will go wrong from now on!" << endl;
 	}
 
