19c9b4265fe70e6a90a31989dc4e629841385ada
Joakim Thorén <joakimthoren93@gmail.com>
2015-04-18 01:19:04 +0200

    improved error message when failed to create targetTexture

--- src/rendersystem.cpp
+++ src/rendersystem.cpp
@@ -68,10 +68,11 @@ RenderSystem::RenderSystem() {
 
 		            SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
 
-			        targetTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 8192, 8192);
+			        targetTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 65532, 65532);
 					if(!targetTexture) {
 						cout << "ERROR: Couldn't create targetTexture!" << endl;
 						cout << "Screen will probably go black from now on" << endl;
+						cout << "SDL_GetError(): " << SDL_GetError() << endl;
 					}
 					SDL_SetRenderTarget(renderer, targetTexture);
 					SDL_RenderClear(renderer);
