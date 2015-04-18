63c1fa5ace002cfc01b9f45733da139be8cdeb69
Joakim Thoren <joakimthoren93@gmail.com>
2015-04-18 15:36:34 +0200

    pass by reference in HashGridSystem::intersect instead of by value

--- include/hashgridsystem.h
+++ include/hashgridsystem.h
@@ -51,7 +51,7 @@ class HashGridSystem : public SpatialIndexer, public ISystem {
 		/**
 		 *	Bounding-box test
 		 **/
-		inline bool intersect(const SpatialIndexer::Rect a, const SpatialIndexer::Rect b) const {
+		inline bool intersect(const SpatialIndexer::Rect& a, const SpatialIndexer::Rect& b) const {
 			return !(a.x + a.w < b.x || a.x > b.x + b.w || a.y + a.h < b.y || a.y > b.y + b.h);
 		}
 
